98e43a9d7f3abd75ddc7596c8af592c822489b2d
Vivek Mishra <vivekmishraiit@gmail.com>
2021-01-08 00:08:26 +0530

    Components Part 2

--- Week 1/Angular/conFusion/src/app/app.module.ts
+++ Week 1/Angular/conFusion/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +24,9 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule
